445d133747734ef94583b90d1ab18f10f68f1e2f
James S White <james@jameswhite.org>
2011-01-18 17:43:05 -0600

    DNS Resolver Model

---
+++ lib/smegdap/Model/DNSResolver.pm
@@ -0,0 +1,27 @@
+package smegdap::Model::DNSResolver;
+
+use strict;
+use warnings;
+use parent 'Catalyst::Model';
+use Net::DNS
+
+=head1 NAME
+
+smegdap::Model::DNSResolver - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+James White
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
