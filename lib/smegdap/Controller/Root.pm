3dbe2027dbf87bfb6aee32bc4ad0ae6f27ecedf8
James S White <james@jameswhite.org>
2011-01-18 18:07:36 -0600

    DNS Resolver Model

--- lib/smegdap/Controller/Root.pm
+++ lib/smegdap/Controller/Root.pm
@@ -106,6 +106,7 @@ sub jstreemenu : Local {
     my @connections; 
     $c->model('DNSResolver')->domain("websages.com");
     push( @connections, $c->model('DNSResolver')->srv("_ldap._tcp") );
+    push( @connections, $c->model('DNSResolver')->srv("_ldap._tls") );
     print STDERR Data::Dumper->Dump([$c->model('DNSResolver')->domain]);
     print STDERR Data::Dumper->Dump([@connections]);
     push( @{ $menu_tree }, [
